c0d9ac4acb677c030dc998d23ae7fdf76d24ee69
juanfparra12 <31254711+juanfparra12@users.noreply.github.com>
2017-09-06 17:04:05 -0400

    Update listingController.js

--- listingController.js
+++ listingController.js
@@ -1,14 +1,34 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
-  function($scope, Listings) {
-    $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
+    function ($scope, Listings)
+    {
+        $scope.listings = Listings;
+        $scope.detailedInfo = undefined;
+        //clears the text boxes after adding to directoryApp
+        $scope.msgdisp = false;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
-  }
+
+        $scope.addListing = function ()
+        {
+            $scope.listings.splice(0, 0, $scope.newListing);
+            $scope.resetModel();
+            $scope.msgdisp = true;
+            $scope.addListingForm.$setPristine();
+        };
+        $scope.deleteListing = function (listing)
+        {
+            $scope.detailedInfo = undefined;
+            $scope.listings.splice($scope.listings.indexOf(listing), 1);
+            $scope.msgdisp = true;
+        };
+        $scope.showDetails = function (listing)
+        {
+            $scope.detailedInfo = Listings[$scope.listings.indexOf(listing)];
+            $scope.msgdisp = false;
+        };
+        $scope.resetModel = function ()
+        {
+            $scope.newListing = {};
+            $scope.msgdisp = false;
+        };
+    }
 ]);
