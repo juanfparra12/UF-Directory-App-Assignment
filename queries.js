9906c2b45abfa91bde57fa997c7b83189c5ea7fb
juanfparra12 <31254711+juanfparra12@users.noreply.github.com>
2017-09-06 17:33:44 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -22,51 +22,55 @@ var findLibraryWest = function() {
 	  if (err)
       throw err;
 
-	  // print out listing information
 	  console.log(listing);
 	});
 };
-var removeCable = function() {
+var removeCable = function()
+{
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console.
    */
-   Listing.findOneAndRemove({ code: 'CABL' }, function(err) {
-	  if (err)
+   Listing.findOneAndRemove({ code: 'CABL' }, function(err)
+   {
+	   if (err)
       throw err;
 
-	  console.log('CABL has been deleted!');
-	});
+	   console.log('CABL has been deleted!');
+	 });
 };
-var updatePhelpsLab = function() {
+var updatePhelpsLab = function()
+{
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then
     log the updated document to the console.
    */
-   Listing.findOneAndUpdate({ code: 'PHL' }, { address: 'New Address' }, function(err, listing) {
-	  if (err) 
-      throw err;		//changes address of PHL (New Address is used because updated address was not given in assignment
-	});
+   Listing.findOneAndUpdate({ code: 'PHL' }, { address: 'New Address' }, function(err, listing)
+   {
+	   if (err)
+       throw err;
+	 });
 
-	Listing.find({ code: 'PHL' }, function(err, listing) {
-	  if (err) 
+	Listing.find({ code: 'PHL' }, function(err, listing)
+  {
+	  if (err)
       throw err;
 
-	  //Displays the 
 	  console.log(listing);
 	});
 };
-var retrieveAllListings = function() {
+var retrieveAllListings = function()
+{
   /*
     Retrieve all listings in the database, and log them to the console.
    */
-   Listing.find({}, function(err, listings) {
-   if (err) 
-    throw err;
+   Listing.find({}, function(err, listings)
+   {
+     if (err)
+      throw err;
 
-   //Displays all the updated listings from the JSON file on the terminal 
-   console.log(listings);
+     console.log(listings);
 	});
 };
 
