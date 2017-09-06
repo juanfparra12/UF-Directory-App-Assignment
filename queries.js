0e3b393b2d74b8982bd084e015f05a5f9b7e5f9c
juanfparra12 <31254711+juanfparra12@users.noreply.github.com>
2017-09-06 17:14:31 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,86 +1,73 @@
 /* Fill out these functions using Mongoose queries*/
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    listings = require('./listings.json'),
-    config = require('./config.js');
-    
-    /* Connect to your database */
-    mongoose.connect(config.db.uri,
-    {
-      useMongoClient: true,
-    });
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 
-
-var findLibraryWest = function() 
+/* Connect to your database */
+mongoose.connect(config.db.uri,
 {
-  /* 
+  useMongoClient: true,
+});
+
+
+
+var findLibraryWest = function() {
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
-   Listing.find({name: "Library West"},function(err,listing)
-   {
-     if(err)
+   Listing.find({ name: 'Library West' }, function(err, listing) {
+	  if (err)
       throw err;
-     console.log(listing);
-   })
-};
 
-var removeCable = function() 
-{
+	  // print out listing information
+	  console.log(listing);
+	});
+};
+var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
-
-   Listing.find({"code": "CABL"}, "", function (err, listing) 
-   {
-       if (err)
-           throw err;
-
-       retrieveAllListings();
-
-   });
-};
-
-var updatePhelpsLab = function() 
-{
-  /*
-    Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-   */
-
-   Listing.find({"name": "Phelps Laboratory"}, "", function (err, listing) 
-   {
-        if (err)
-            throw err;
-
-        listing.address = 'Phelps Lab, Gainesville, FL 32611, United States';
-        listing.save(function (err) 
-        {
-            if (err) 
-            {
-                throw err;
-            }
-        });
-
-        console.log(listing);
-    });
-};
-
-var retrieveAllListings = function() 
-{
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
-
-   Listing.find({}, function(err, listing){
-     if(err) 
+   Listing.findOneAndRemove({ code: 'CABL' }, function(err) {
+	  if (err)
       throw err;
-     console.log(listing);
-    });
+
+	  console.log('CABL has been deleted!');
+	});
+};
+var updatePhelpsLab = function() {
+  /*
+    Phelps Laboratory's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
+   */
+   Listing.findOneAndUpdate({ code: 'PHL' }, { address: 'New Address' }, function(err, listing) {
+	  if (err) 
+      throw err;		//changes address of PHL (New Address is used because updated address was not given in assignment
+	});
+
+	Listing.find({ code: 'PHL' }, function(err, listing) {
+	  if (err) 
+      throw err;
+
+	  //Displays the 
+	  console.log(listing);
+	});
+};
+var retrieveAllListings = function() {
+  /*
+    Retrieve all listings in the database, and log them to the console.
+   */
+   Listing.find({}, function(err, listings) {
+   if (err) 
+    throw err;
+
+   //Displays all the updated listings from the JSON file on the terminal 
+   console.log(listings);
+	});
 };
 
 findLibraryWest();
